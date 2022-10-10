06cfc0905fb29c91fed6a3436b71365995f1a6ff
Preeya Singh <Preeya@preeyas-mbp.myfiosgateway.com>
2022-10-10 17:46:38 -0400

    Homework 2

---
+++ Modules/Lab 10-8-22.js
@@ -0,0 +1,3 @@
+let roomTemp = 50;
+let heatStatus = roomTemp <= 60 ? "Turn Heat on" : "Turn Heat off";
+console.log(heatStatus)
--- Modules/Variables.js
+++ Modules/Variables.js
@@ -1,1 +1,33 @@
 console.log("hey ya'll");
+
+
+var1 = 'HappyFish'
+var2 = 8
+console.log(var2)
+var3 = var2+1
+var4 = 'My team is called (var1) and we had (var2) team players and now we have (var3) team players'
+
+console.log(var1)
+
+console.log(var3)
+console.log(var4)
+console.log(`My team is called ${var1} and we had ${var2} team players and now we have ${var3} team players`)
+
+/**
+ * 1. Create a variable named myTeamName and assign any name to your team
+ * 2. create a variable named myTeamSize and assign any number to it
+ * 3. increment the team size by 1
+ * 4. print the result like:
+ *  I am part of ABC team, we have total X-members
+ */
+
+ Assign value to the variable
+ *      if the full-name has more than 14 characters and contains 'els' 
+ *          assign 23 in a variable
+ *      otherwise
+ *          assign 32 in a variable
+ * 
+ let mySentence = 'Pages That you vIeW in tHIs WinDOw wont apPeAr in the broWSer history AND thEY wont LEaVE other TRACes';
+ 
+ let indexOf_w = mySentence.indexOf('w')
+ console.log(`index of 'w' -> ${indexOf_w}`
